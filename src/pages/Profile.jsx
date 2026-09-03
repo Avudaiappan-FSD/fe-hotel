8f4e849217022ce8b2ff3da0ec62ea92ad84828d
Avudaiappan-FSD <harinzz6803@gmail.com>
2026-09-03 12:54:50 +0530

    profile all are done

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,7 +1,6 @@
 import { useLoaderData } from "react-router-dom";
 import edit from "../assets/images/edit.png";
 
-
 const Profile = () => {
 
     const me = useLoaderData();
@@ -9,15 +8,18 @@ const Profile = () => {
     return (
         <div>
             <h1 className="text-2xl font-bold px-3 py-4">Hi, {me.user.name}</h1>
-            <div className="profileheader ">
-                <h1 className="text-2xl font-bold font-size: 25px;">Profile</h1>
-                <img src={edit} alt="Profile" className="w-7 h-7 rounded-full size-22px" />
+            <div className="bg-blue-200 p-5 max-w-md mx-auto rounded-2xl mt-10">
+                <div className="profileheader ">
+                    <h1 className="text-2xl font-bold font-size: 25px;">Profile</h1>
+                    <img src={edit} alt="Profile" className="w-7 h-7 rounded-full size-22px" />
+                </div>
+
+                <div className="bg-white shadow-md rounded-lg p-2">
+                    <h2 className="text-xl font-semibold">Name: {me.user.name}</h2>
+                    <p className="text-gray-700">Email: {me.user.email}</p>
+                </div>
             </div>
 
-            <div className="bg-white shadow-md rounded-lg p-2">
-                <h2 className="text-xl font-semibold">Name: {me.user.name}</h2>
-                <p className="text-gray-700">Email: {me.user.email}</p>
-            </div>
         </div>
     )
 }
